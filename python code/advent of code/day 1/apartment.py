93a7540a2a137b17c6c339beea8762d2b13a1864
devmart10 <devmart10@gmail.com>
2016-07-13 15:28:51 -0700

    Part 2

--- python code/advent of code/day 1/apartment.py
+++ python code/advent of code/day 1/apartment.py
@@ -1,10 +1,15 @@
 with open('input.txt', 'r') as f:
 	line = f.readline()
 	i = 0
+	c = 0
 	for x in line:
+		c += 1
 		if(x == '('):
 			i += 1
 		else:
 			i -= 1
 
-	print('floor:', i)
+		if i < 0:
+			break;
+
+	print('char:', c)
