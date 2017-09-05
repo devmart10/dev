c3442a25e52aefbdd38be3aec986fe4d257cbfd5
Devon Martin <devmart10@gmail.com>
2017-09-05 08:46:37 -0700

    removes old code

--- python code/noise violation program/funcs.py
+++
@@ -1,9 +0,0 @@
-def get_street(address):
-	temp = address.split(' ')
-	return temp[1] + ' ' + temp[2]
-
-def add_street(street, database):
-	if database.has_key(street):
-		database[street] += 1
-	else:
-		database[street] = 0
